e5a907aa1abb493a5b2d56c3ab286c4420da1d2f
csd5766 <csd5766@naver.com>
2018-04-17 15:01:15 +0900

    Alert

--- UIAlert03/ViewController.swift
+++ UIAlert03/ViewController.swift
@@ -15,11 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btn(_ sender: Any) {
+    
+        let myAlert = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다.", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.red
+            })
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.white
+        })
+        
+        let testAcotion = UIAlertAction(title: "Delete", style: .destructive, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.green
+        })
+        
+        myAlert.addAction(okAction)
+        myAlert.addAction(cancelAction)
+        myAlert.addAction(testAcotion)
+        
+        present(myAlert, animated:true, completion: nil)
+        }
+    
     }
+    
 
 
-}
+
+
 
